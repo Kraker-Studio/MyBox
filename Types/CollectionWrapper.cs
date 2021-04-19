8f1cf1de9a9cacb1c6bea7a056cae76ebbe86089
M. Emin Öner <m.emin.oner@gmail.com>
2021-04-19 21:19:14 +0300

    List features added to collection wrapper for convenience.

--- Types/CollectionWrapper.cs
+++ Types/CollectionWrapper.cs
@@ -1,44 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using MyBox.Internal;
 
-namespace MyBox
-{
-	/// <summary>
-	/// CollectionWrapper used to apply custom drawers to Array fields
-	/// </summary>
-	[Serializable]
-	public class CollectionWrapper<T> : CollectionWrapperBase
-	{
-		public T[] Value;
-	}
+namespace MyBox {
+    /// <summary>
+    /// CollectionWrapper used to apply custom drawers to Array fields
+    /// </summary>
+    [Serializable]
+    public class CollectionWrapper<T> : CollectionWrapperBase {
+        public T[] Value;
+
+        public T this[int i] {
+            get { return Value[i]; }
+            set { Value[i] = value; }
+        }
+        public int Count {
+            get {
+                return Value.Length;
+            }
+        }
+        public List<T> To_List(){
+            return new List<T>(Value);
+        }
+    }
 }
 
-namespace MyBox.Internal
-{
-	[Serializable]
-	public class CollectionWrapperBase {}
+namespace MyBox.Internal {
+    [Serializable]
+    public class CollectionWrapperBase { }
 }
 
 #if UNITY_EDITOR
-namespace MyBox.Internal
-{
-	using UnityEditor;
-	using UnityEngine;
-	
-	[CustomPropertyDrawer(typeof(CollectionWrapperBase), true)]
-	public class CollectionWrapperDrawer : PropertyDrawer
-	{
-		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-		{
-			var collection = property.FindPropertyRelative("Value");
-			return EditorGUI.GetPropertyHeight(collection, true);
-		}
+namespace MyBox.Internal {
+    using UnityEditor;
+    using UnityEngine;
 
-		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-		{
-			var collection = property.FindPropertyRelative("Value");
-			EditorGUI.PropertyField(position, collection, label, true);
-		}
-	}
+    [CustomPropertyDrawer(typeof(CollectionWrapperBase), true)]
+    public class CollectionWrapperDrawer : PropertyDrawer {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            var collection = property.FindPropertyRelative("Value");
+            return EditorGUI.GetPropertyHeight(collection, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            var collection = property.FindPropertyRelative("Value");
+            EditorGUI.PropertyField(position, collection, label, true);
+        }
+    }
 }
 #endif
